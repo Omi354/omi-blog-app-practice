34a2d80a1c3496daeb2cd003729d204d5ab8c53f
Omi354 <bell.tsaeb.shoji+work@gmail.com>
2025-02-18 00:59:01 +0900

    feat: 新規投稿機能を実装しました

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,4 +12,20 @@ class ArticlesController < ApplicationController
   def new
     @article = current_user.articles.build
   end
+
+  def create
+    @article = current_user.articles.build(article_params)
+    if @article.save
+      flash[:notice] = "投稿が完了しました"
+      redirect_to article_path(@article)
+    else
+      flash.now[:error] = "投稿に失敗しました"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :content, :eye_catch)
+  end
 end
