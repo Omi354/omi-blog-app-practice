6061ccd6c2c091776cbcc53da7a6adfbc53a8848
Omi354 <bell.tsaeb.shoji+work@gmail.com>
2025-02-17 15:07:12 +0900

    feat: deviseを使用し、ユーザーモデルの作成とマイグレーションを行いました。また、適切にビューが表示されるようにcontroller、view、deviseの設定を変更しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: "users/sessions"
+  }
   get "up" => "rails/health#show", as: :rails_health_check
   root to: "home#index"
-  
 
 end
