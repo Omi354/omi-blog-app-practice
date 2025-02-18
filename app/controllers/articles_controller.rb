1ea80e65eb16a624bb8083ee9eb08e7ab8911221
Omi354 <bell.tsaeb.shoji+work@gmail.com>
2025-02-18 11:44:03 +0900

    refactor: articles_controllerのリファクタリングを行いました

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,13 @@
 class ArticlesController < ApplicationController
-  before_action :authenticate_user!, only: [ :new, :create, :edit, :update ]
+  before_action :authenticate_user!, only: [ :new, :create, :edit, :update, :destroy ]
+  before_action :set_article, only: [ :show ]
+  before_action :set_user_article, only: [ :edit, :update, :destroy ]
 
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -25,11 +26,9 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = current_user.articles.find(params[:id])
   end
 
   def update
-    @article = current_user.articles.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "更新に成功しました"
       redirect_to article_path(@article)
@@ -40,8 +39,7 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    article = current_user.articles.find(params[:id])
-    article.destroy!
+    @article.destroy!
     flash[:notice] = "記事を削除しました"
     redirect_to articles_path, status: :see_other
   end
@@ -49,5 +47,13 @@ class ArticlesController < ApplicationController
   private
   def article_params
     params.require(:article).permit(:title, :content, :eye_catch)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def set_user_article
+    @article = current_user.articles.find(params[:id])
   end
 end
