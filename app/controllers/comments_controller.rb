2d421cb34121799d6c7ffc78fe732fc4033bdbe9
Omi354 <bell.tsaeb.shoji+work@gmail.com>
2025-02-19 00:40:48 +0900

    feat: commentのnewアクションとコメント画面を作成しました

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
 
+  def new
+    @comment = current_user.comments.build(article_id: params[:article_id])
+  end
 end
