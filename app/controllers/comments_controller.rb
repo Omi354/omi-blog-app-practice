402a73dccc369e38c34dc825de383a88e7d8f765
Omi354 <bell.tsaeb.shoji+work@gmail.com>
2025-02-20 00:15:51 +0900

    feat: コメントの削除機能を実装しました

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_comment, only: [ :destroy ]
+  before_action :authorize_destroy, only: [ :destroy ]
 
   def new
     @comment = current_user.comments.build(article_id: params[:article_id])
@@ -18,10 +20,27 @@ class CommentsController < ApplicationController
 
   end
 
+  def destroy
+    @comment.destroy!
+    flash[:notice] = "コメントを削除しました"
+    redirect_to article_path(@comment.article), status: :see_other
+  end
+
   private
 
   def comment_params
     params.require(:comment).permit(:content)
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def authorize_destroy
+    unless current_user == @comment.user || current_user == @comment.article.user
+      flash[:error] = "削除権限がありません"
+      redirect_to article_path(@comment.article)
+    end
   end
 
 end
