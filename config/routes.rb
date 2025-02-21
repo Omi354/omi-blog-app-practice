0e0f4f3bd83ee4cdc30fdecfa78c25e039c01ec2
Omi354 <bell.tsaeb.shoji+work@gmail.com>
2025-02-22 00:38:58 +0900

    lint: rubocopを適用しました

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
   root to: "articles#index"
   resources :articles do
     resources :comments, only: [ :new, :create, :destroy ]
-    resource :likes, only: [:create, :destroy]
+    resource :likes, only: [ :create, :destroy ]
   end
 
-  get 'favorites', to: 'favorites#index'
+  get "favorites", to: "favorites#index"
   resource :profile, only: [ :show, :edit, :update ]
 end
