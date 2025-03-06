da2e6555a858e27ed09f1e7c7aaefceb5f1a8424
Omi354 <bell.tsaeb.shoji+work@gmail.com>
2025-03-06 13:44:17 +0900

    chore: jqueryとaxiosを使用できるように設定しました

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,53 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import jquery from "jquery"
+import "axios"
+window.$ = jquery
+
+
+// Turbo Driveのロードイベントで実行される関数
+document.addEventListener('turbo:load', function() {
+  console.log('Page loaded with Turbo Drive');
+  $('.article_title').click(() => {
+    alert("clicked")
+  })
+
+  // axiosを使ったGETリクエストのサンプル
+  console.log('Sending test axios GET request...');
+  axios.get('/articles')
+    .then(response => {
+      console.log('Axios request successful');
+      console.log('Data received:', response.data);
+      
+      // データを使って何か処理をする例
+      const articleCount = response.data.length || 0;
+      console.log(`Found ${articleCount} articles`);
+      
+      // 取得したデータをUIに表示する例
+      $('#axios-response-container').html(`<p>Loaded ${articleCount} articles via axios</p>`);
+    })
+    .catch(error => {
+      console.error('Axios request failed:', error);
+      $('#axios-response-container').html('<p class="text-danger">Failed to load data</p>');
+    })
+    .finally(() => {
+      console.log('Axios request completed');
+    });
+  
+  // ここに初期化コードやページ読み込み時に実行したいコードを記述します
+  // 例：
+  // $('.some-element').on('click', function() {
+  //   console.log('Element clicked!');
+  // });
+  
+  // Ajaxリクエストの例
+  // axios.get('/api/data')
+  //   .then(response => {
+  //     console.log('Data loaded:', response.data);
+  //   })
+  //   .catch(error => {
+  //     console.error('Error loading data:', error);
+  //   });
+});
+
